06c62766205a1d4650f90a12ce685fae4c6d33cf
Donard97 <donardgolaj@gmail.com>
2021-11-17 00:31:15 +0100

    Set an instance and add method validate_name

--- person.rb
+++ person.rb
@@ -1,18 +1,26 @@
+require './corrector.rb'
+
 class Person
   attr_accessor :name, :age
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
-    @id = Random.ran(1..1000)
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @corrector = Corrector.new
   end
 
   def of_age?
     @age >= 18
   end
 
+  def validate_name
+    validate = @corrector
+    @name = validate.correct_name(@name)
+  end
+  
   private :of_age?
 
   def can_use_services?
