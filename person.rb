df56fc627303a0afb9b8f6df87a8b95e0c71deca
Donard97 <donardgolaj@gmail.com>
2021-11-16 01:15:16 +0100

    Add private method for age

--- person.rb
+++ person.rb
@@ -1,10 +1,17 @@
 class Person
-attr_accessor :name, :age
-attr_reader :id
-def intialize(name = 'Unknown', age, parent_permission:true)
-  @id = Random.ran(1..1000)
-  @name = name
-  @age = age
-  @parent_permission = parent_permission
-end
+  attr_accessor :name, :age
+  attr_reader :id
+
+  def intialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.ran(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  def is_of_age?
+    @age >= 18
+  end
+
+  private:is_of_age?
 end
