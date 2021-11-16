c91fdbe9fabc18fec82c4785a9bee77c9546e95d
Donard97 <donardgolaj@gmail.com>
2021-11-16 01:33:19 +0100

    Add super keyword

--- student.rb
+++ student.rb
@@ -2,6 +2,7 @@ require './person.rb'
 
 class Student > person
   def initialize(classrom,age, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission)
     @classrom = classroom
   end
 
--- teacher.rb
+++ teacher.rb
@@ -2,6 +2,7 @@ require './person.rb'
 
 class Teacher > Person
   def initialize(specialization, age, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission)
     @specialization = specialization
   end
 end
