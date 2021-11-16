d024a04d6bca54df0a51a119786e70e7b72e810b
Donard97 <donardgolaj@gmail.com>
2021-11-17 00:30:42 +0100

    Create method to make sure that name has max 10 characters

--- corrector.rb
+++ corrector.rb
@@ -1,5 +1,10 @@
 class Corrector 
   def correct_name(name)
     given_name = name.strip.capitalize
+    if given_name.length > 10
+      given_name[0, 10]
+    else
+      given_name
+    end
   end  
 end
