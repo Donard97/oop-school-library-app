e752ebca578734d02cda78bbc590dd356ac19435
Donard97 <donardgolaj@gmail.com>
2021-11-18 00:07:29 +0100

    Create the has-many relationship of Book and Rental

--- book.rb
+++ book.rb
@@ -1,7 +1,14 @@
 class Book
   attr_accessor :title, :author
+  attr_reader: :rentals, :Book
+
   def initialize(title, author)
     @title  = title
     @author = author
+    @rentals = []
+  end
+
+  def add_rental(rental)
+    @rentals.push(rental)
   end
 end
