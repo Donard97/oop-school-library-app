34f4f3854ff1d017d3c4ce504469236090121fde
Donard97 <donardgolaj@gmail.com>
2021-11-18 00:13:47 +0100

    Modify constructor of Rental to add Book and Person

--- rental.rb
+++ rental.rb
@@ -1,7 +1,13 @@
 class Rental
   attr_accessor :date, :book, :person
   
-  def initialize(date)
+  def initialize(date, person, book)
     @date = date
+
+    @person = person
+    person.rentals << self
+
+    @book = book
+    book.rentals << self
   end
 end
