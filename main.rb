c4f059847f25e1d630e60bbdae0e2f137e245ae3
Donard97 <donardgolaj@gmail.com>
2021-11-18 23:42:57 +0100

    Add an input with options

--- main.rb
+++ main.rb
@@ -31,4 +31,24 @@ class App
     option = gets.chomp
     get_option option
   end
+
+  def get_option(input)
+    case input
+    when '1'
+      list_all_books
+    when '2'
+      list_all_people
+    when '3'
+      create_a_person
+    when '4'
+      create_a_book
+    when '5'
+      create_a_rental
+    when '6'
+      list_rentals_by_person_id
+    when '7'
+      puts 'Thank you for using our library'
+    else
+      puts 'Please enter a number between 1 and 7'
+    end
 end
