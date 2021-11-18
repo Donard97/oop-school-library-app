a25a96acb3fd747edf3666387334730856a60dfa
Donard97 <donardgolaj@gmail.com>
2021-11-18 23:26:49 +0100

    Create menu list

--- main.rb
+++ main.rb
@@ -12,4 +12,23 @@ class App
     @rentals = []
     @class = Classroom.new('Grade 7')
   end
+
+  def run 
+    print 'Welcome to School Library App'
+    sleep 0.8
+    menu
+  end
+
+  def menu
+    puts 'Please choose an option by entering a number'
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts '7 - Exit'
+    option = gets.chomp
+    get_option option
+  end
 end
