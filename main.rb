7ff0f903af8812d115926030f175a0c8fe1cb60a
Donard97 <donardgolaj@gmail.com>
2021-11-18 23:21:52 +0100

    Initialize the App class

--- main.rb
+++ main.rb
@@ -5,5 +5,11 @@ require './person.rb'
 require './student.rb'
 require './teacher.rb'
 
-class app
+class App
+  def initialize 
+    @people = []
+    @books = []
+    @rentals = []
+    @class = Classroom.new('Grade 7')
+  end
 end
