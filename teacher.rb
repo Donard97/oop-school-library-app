2e05c0f27f4470f841dc584376581f93366f5bba
olipliche <olipliche@gmail.com>
2021-11-25 13:18:10 +0100

    Update teacher.rb file

--- teacher.rb
+++ teacher.rb
@@ -2,6 +2,8 @@
 require_relative './person'
 
 class Teacher < Person
+  attr_accessor :specialization
+
   def initialize(specialization, age, name = 'Unknown', parent_permission = true)
     super(age, name, parent_permission)
     @specialization = specialization
