af530bf8c709b8c46ef4fc9e5461e34708f6c63f
olipliche <olipliche@gmail.com>
2021-11-25 14:53:54 +0100

    Add student test

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -1,13 +1,22 @@
 require_relative '../classroom'
+require_relative '../student'
 
 describe Classroom do
   before :each do
     @classroom = Classroom.new('Learn to code')
+    @student = Student.new(age: 19, classroom: @classroom.label, name: 'Donard', parent_permission: true)
   end
 
   describe '#new' do
     it 'takes one parameter and returns a Classroom object' do
       expect(@classroom).to be_an_instance_of Classroom
+    end
+  end
+
+  describe '#new' do
+    it 'add student details to student array' do
+      @classroom.add_student(@student)
+      expect(@classroom.students).to include@student
     end
   end
 
